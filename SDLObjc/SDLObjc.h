d569e512cfb7a733a26cf7bfa64e59fe475c83ab
Freddy Kellison-Linn <fred.kl@me.com>
2014-05-26 14:11:05 -0700

    Initial Commit

---
+++ SDLObjc/SDLObjc.h
@@ -0,0 +1,13 @@
+//
+//  SDLObjc.h
+//  SDLObjc
+//
+//  Created by Freddy Kellison-Linn on 5/26/14.
+//  Copyright (c) 2014 Jumhyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDLObjc : NSObject
+
+@end
---
+++ SDLObjc/SDLObjc.m
@@ -0,0 +1,13 @@
+//
+//  SDLObjc.m
+//  SDLObjc
+//
+//  Created by Freddy Kellison-Linn on 5/26/14.
+//  Copyright (c) 2014 Jumhyn. All rights reserved.
+//
+
+#import "SDLObjc.h"
+
+@implementation SDLObjc
+
+@end
